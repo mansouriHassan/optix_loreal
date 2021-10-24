93cb52e1136404330da224b5569ff93e3e1bb9e5
MANSOURI Hassan - INFOGENE <hassan.mansouri@rd.loreal.com>
2021-10-24 20:27:39 +0200

    add optix app

--- 3rdparty.cmake
+++ 3rdparty.cmake
@@ -83,13 +83,13 @@ endif()
 
 message("Creating 3rdparty library folder for ${GENERATOR} ${BUILD_ARCH}")
 
-set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_SOURCE_DIR}/optix-hair-7.1/3rdparty" CACHE PATH "default install path" FORCE)
+set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty" CACHE PATH "default install path" FORCE)
 
-set(DOWNLOAD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/optix-hair-7.1/3rdparty/downloads")
-set(PATCH_DIR    "${CMAKE_CURRENT_SOURCE_DIR}/optix-hair-7.1/3rdparty/patches")
+set(DOWNLOAD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/downloads")
+set(PATCH_DIR    "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/patches")
 
-set(SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/optix-hair-7.1/temp/sources")
-set(BUILD_DIR  "${CMAKE_CURRENT_SOURCE_DIR}/optix-hair-7.1/temp/build/${MSVC_TOOLSET}")
+set(SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/temp/sources")
+set(BUILD_DIR  "${CMAKE_CURRENT_SOURCE_DIR}/temp/build/${MSVC_TOOLSET}")
 
 message("Install prefix: ${CMAKE_INSTALL_PREFIX} ${ARGC} ${ARGV}")
 
--- 3rdparty.cmd
+++ 3rdparty.cmd
@@ -1,1 +1,1 @@
-"E:\\Jenkins\\tools\\hudson.plugins.cmake.CmakeTool\\3.21.2\\bin\\cmake.exe" -P optix-hair-7.1\3rdparty.cmake -DCMAKE_INSTALL_PREFIX="optix-hair-7.1\\3rdparty"
+"C:\Program Files\cmake\bin\cmake.exe" -P 3rdparty.cmake -DCMAKE_INSTALL_PREFIX="3rdparty"
